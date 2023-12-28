02af4809547113e43af959a35bbb8e96f1ac08d0
Mehmet Imga <imga@ruststreetinteractive.com>
2023-12-28 09:40:42 -0700

    first commit

---
+++ blog.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  url: "https://mehmetimga-blog.vercel.app/",
+  title: "Mehmet Imga Personal Blog",
+  subtitle: "Content authoring with MDX. Powered by Next.js.", // longDescription : appears in landing page meta description
+  description:
+    "Next.js MDX Personal Blog.",
+  author: "John Polacek",
+  analytics: "", // put your google analytics code here to add tracking
+  shareImage: "https://mehmetimga-blog.vercel.app/img/programmer.png",
+  shareImageAlt: "Personal Blog",
+  postsPerPage: 8,
+  showDate: true, // if you would like the date to appear on the blog posts
+  disqus: "", // your disqus shortname, remove if you don't use disqus
+}
